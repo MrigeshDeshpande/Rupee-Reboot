008cf1cdaf55a367a2fa7e6edb27f4ec03e159bc
Mrigesh Deshpande <mrigeshdeshpande246+code@gmail.com>
2025-04-07 21:04:37 +0530

    Landing Page - rolled out

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-
+import LandingPage from './components/LandingPage.jsx';
+import React from 'react';
 function App() {
 
   return (
-    <div className="min-h-screen bg-gradient-to-br from-[#F9F9FB] to-[#E0F7FA] flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-blue-600">RupeeReboot is Live 💸</h1>
-    </div>
+  <>
+  <LandingPage />;
+  
+  </>
+      
+
+    
   )
 }
 
