e8596eaa47eed35990e44e17cc18e69fe5338cd0
Mrigesh Deshpande <mrigeshdeshpande246+code@gmail.com>
2025-04-07 16:28:54 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-[#F9F9FB] to-[#E0F7FA] flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-600">RupeeReboot is Live 💸</h1>
+    </div>
+  )
+}
+
+export default App
