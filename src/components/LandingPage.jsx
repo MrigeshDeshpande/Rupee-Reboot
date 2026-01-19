760b81709818c93a66b8b2238fd0982bfc661a77
Mrigesh Deshpande <mrigeshdeshpande246+code@gmail.com>
2026-01-19 07:18:23 +0530

    back in the game

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../styles/LandingPage.css";
+import "../Styles/LandingPage.css";
 import SuccessFactorsImg from "../assets/ok.svg";
 import { Link } from "react-router-dom";
 
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../styles/Auth.css";
+import "../Styles/Auth.css";
 import { Link } from "react-router-dom";
 
 const Signup = () => {
