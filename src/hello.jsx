41c60860f088b15c49d66bf39a21da2a4d23847c
Mrigesh Deshpande <mrigeshdeshpande246+code@gmail.com>
2026-02-08 19:04:47 +0530

    clean setup tailwind added

---
+++ src/hello.jsx
@@ -0,0 +1,11 @@
+const Hello = () => {
+  return (
+    <>
+      <div className="min-h-screen bg-red-500 flex items-center justify-center">
+        <h1 className="text-white text-4xl font-bold">Rupee-Reboot</h1>
+      </div>
+    </>
+  );
+};
+
+export default Hello;
